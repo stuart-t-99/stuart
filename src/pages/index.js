046390c5b8300e8e7bc9f61ecee6fddc4c20f1e4
stuart-t-99 <79085043+stuart-t-99@users.noreply.github.com>
2021-07-12 13:26:18 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h2>Strategy | Analytics | Machine Learning | Automation</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
