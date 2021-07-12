175449445123398a7b8dc96d95d57ea103880243
stuart-t-99 <79085043+stuart-t-99@users.noreply.github.com>
2021-07-12 14:47:45 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <p>I have over 10 years combined experience in Strategy, Analytics and implementing ML solutions that solve real customer problems.</p>
     <p>Currently managing a large project for the implementation of Microsoft Azure Machine Learning.</p>
     <p>Passionate about getting MLOps frameworks setup to allow ML solutions to scale.</p>
-    <h3>Certified Micosoft Azure Data Scientist.</h3>
+    <h3>Certified Micosoft Azure Data Scientist</h3>
       <StaticImage
       src="../images/azure-data-scientist-associate-600x600.png"
       width={100}
@@ -21,16 +21,44 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
-   <h3>Certified LUMA Human Centred Design Practitioner.</h3>
+   <h3>Certified LUMA Human Centred Design Practitioner</h3>
 <StaticImage
       src="../images/luma2.png"
       width={100}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="Product"
+      style={{ marginBottom: `1.45rem` }}
+    />
+<h3>RMIT Product Management</h3>
+<StaticImage
+      src="../images/RMIT_prod.png"
+      width={100}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="Product"
+      style={{ marginBottom: `1.45rem` }}
+    />
+<h3>Certified SAFe® 5 Product Owner/Product Manager</h3>
+<StaticImage
+      src="../images/scaled_ag.png"
+      width={100}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="Product"
+      style={{ marginBottom: `1.45rem` }}
+    />
+<h3>Bachelor Chemical Engineering (Hons.) Bachelor of Science</h3>
+<StaticImage
+      src="../images/The-University-of-Melbourne-Logo.png"
+      width={100}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="Product"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
+      
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
